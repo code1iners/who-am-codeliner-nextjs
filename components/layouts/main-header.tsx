c065f9a6cbf1046ecc8cafa4d86d3a9f1f4a7afe
WONI <codeliner96@fixelsoft.com>
2022-06-13 14:32:20 +0900

    Implement main header.

--- components/layouts/main-header.tsx
+++ components/layouts/main-header.tsx
@@ -1,3 +1,26 @@
 export default function MainHeader() {
-  return <header>Header</header>;
+  return (
+    <header className="px-2 py-6 flex items-center justify-between">
+      <div className="flex items-center gap-1 cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+          />
+        </svg>
+        <h1 className="font-bold">Who Am Codeliner</h1>
+      </div>
+      <div className="font-semibold tracking-wider text-sm">
+        Codeliner@gmail.com
+      </div>
+    </header>
+  );
 }
