973115fda4064ac4439e5c368fb6250681baa49b
WONI <codeliner96@fixelsoft.com>
2022-06-14 11:26:24 +0900

    Added favicons.

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import MainFavicon from "../components/main-favicon";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <MainFavicon />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
