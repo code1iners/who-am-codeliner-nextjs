742215a577b023878a9519db6f34024ec0e02cf7
김원이 <codeliner96@fixelsoft.com>
2023-02-01 13:10:12 +0900

    Exclude where is side project.

--- pages/api/v1/projects.ts
+++ pages/api/v1/projects.ts
@@ -24,13 +24,13 @@ const sideProjects: SideProject[] = [
     skills: ["JS", "TS", "NPM"],
     url: "https://github.com/code1iners/ce1pers",
   },
-  {
-    id: 2,
-    name: "Where is",
-    icon: "M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7",
-    skills: ["NextJS", "Typescript"],
-    url: "https://where-is.codeliners.cc",
-  },
+  // {
+  //   id: 2,
+  //   name: "Where is",
+  //   icon: "M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7",
+  //   skills: ["NextJS", "Typescript"],
+  //   url: "https://where-is.codeliners.cc",
+  // },
 ];
 
 export default function handler(
